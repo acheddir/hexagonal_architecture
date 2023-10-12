11d24c6b420b9ef39fdb7c7c1a749ed86a48b3c5
Abderrahman Cheddir <acheddir@redsen.ch>
2023-10-12 14:22:11 +0100

    test: missing files

--- test/UnitTests/Core/UnitTests.Application/GetAccountBalanceQuery.Tests.cs
+++ test/UnitTests/Core/UnitTests.Application/GetAccountBalanceQuery.Tests.cs
@@ -1,13 +1,9 @@
-﻿using Me.Acheddir.Hexagonal.Application.Ports.Driven;
-using Me.Acheddir.Hexagonal.Application.UseCases.GetAccountBalance;
-using Moq;
-
-namespace Tests;
+﻿namespace Me.Acheddir.Hexagonal.UnitTests.Application;
 
 public class GetAccountBalanceQueryTests
 {
     [Fact]
-    public async void GivenAccountId_WhenGetAccountBalanceQueryIsIssued_ThenHandlerShouldReturnCorrectBalance()
+    public async Task GivenAccountId_WhenGetAccountBalanceQueryIsIssued_ThenHandlerShouldReturnCorrectBalance()
     {
         // Arrange
         var accountId = new AccountId(1);
@@ -24,20 +20,22 @@ public class GetAccountBalanceQueryTests
                     .WithMoney(Money.Of(1)).Build()))
             .Build();
 
-        var accountLoad = new Mock<IAccountLoad>();
-        accountLoad
+        var accountLoadMock = new Mock<IAccountLoad>();
+        accountLoadMock
             .Setup(
                 al => al.LoadAccountAsync(
                     It.IsAny<AccountId>(),
                     It.IsAny<DateTime>(),
                     It.IsAny<CancellationToken>()))
-            .Returns(Task.FromResult(account));
+            .ReturnsAsync(account);
 
         var query = new GetAccountBalanceQuery {AccountId = new AccountId(1)};
-        var handler = new GetAccountBalanceQueryHandler(accountLoad.Object);
+        var handler = new GetAccountBalanceQueryHandler(accountLoadMock.Object);
 
+        // Act
         var result = await handler.Handle(query, CancellationToken.None);
 
+        // Assert
         result.Value.Should().Be(Money.Of(1555));
     }
 }
